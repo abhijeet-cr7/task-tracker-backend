149b9aa81eadd1bcbd07b83ec2dbafcbbd69ffd0
abhijeet-cr7 <abhijeetdbz7@gmail.com>
2024-07-28 19:38:45 +0530

    added mongo uri in .env file

--- modal/database.js
+++ modal/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(`mongodb+srv://abhijeet:mukherjee@cluster0.lwi4805.mongodb.net/`, {useNewUrlParser : true});
+        const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {useNewUrlParser : true});
         console.log(`MongoDB Connected : {conn.connection.host}`);
     }
     catch(error){
